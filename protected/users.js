f5042e13f1cf40bfcead1601eebaa93cdf71d8a8
galalahmad31-cpu <galalahmad31@gmail.com>
2026-08-20 02:48:06 +0300

    Update users.js

--- protected/users.js
+++ protected/users.js
@@ -3,7 +3,7 @@ window.USERS_DATABASE = {
     "admin@example.com": {
         password: "admin@2468",
         startDate: "2026-08-19",
-        expireDate: "2026-08-31",
+        expireDate: "2026-07-31",
         maxDevices: 1
     }
 
