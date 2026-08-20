79fafc27f71702188a8f4f696c01eead3bd8e270
galalahmad31-cpu <galalahmad31@gmail.com>
2026-08-20 18:43:46 +0300

    Update users.js

--- protected/users.js
+++ protected/users.js
@@ -1,7 +1,7 @@
 window.USERS_DATABASE = {
 
-    "admin@gmail.com": {
-        password: "admin@2468",
+    "nsc@gmail.com": {
+        password: "nsc@2468",
         startDate: "2026-08-19",
         expireDate: "2026-08-31",
         maxDevices: 100
